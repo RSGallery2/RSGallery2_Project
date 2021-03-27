6ffb9ff74ccb261ee83dcf89b7ccbbb51a94f41b
ThomasFinnern <ThomasFinnernDell5540@users.noreply.github.com>
2021-03-27 18:39:20 +0100

    Table of content is started

--- Documentation/J!4x/RSGallery2_documentation_J4x.bat
+++ Documentation/J!4x/RSGallery2_documentation_J4x.bat
@@ -5,8 +5,8 @@ ECHO.
 
 REM does use the python plugin markdown-pp to collect 
 REM all *.md files and create RSGallery2.Documentation.md
-REM file.
-REM In Atom use the plugin markdown to PDF to get the matching PDF
+REM file. 
+REM In Atom use the plugin markdown to PDF to create the matching *.PDF
 
 markdown-pp RSGallery2_documentation_J4x.mdpp -o RSGallery2_documentation_J4x.md -e latexrender
 
